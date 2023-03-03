984b88993093b990f83369c647a504bc907b2f73
kadeem254 <kadeemalexander254@gmail.com>
2023-03-03 12:45:25 +0300

    No Comment

--- algorithms.mjs
+++ algorithms.mjs
@@ -98,8 +98,123 @@ function isPrimeNumber( number ){
   return is_prime;
 }
 
+/**Tests if the input number is a valid power of 2.
+ * Big O => O(log n);
+ * @param {number} num - the number to test
+ * @returns {boolean}
+*/
+function isPowerOfTwo( num ){
+  // edge case '1'
+  if( num < 1 ) return false;
+
+  while( num > 1 ){
+    // check if divisible by 2
+    if( num % 2 != 0 ) return false;
+
+    // update the value of num
+    num /= 2;
+  }
+  return true;
+}
+
+/**Tests if the input number is a valid power of 2.
+ * Big O => O(1);
+ * @param {number} num - the number to test
+ * @returns {boolean}
+*/
+function isPowerOfTwoBitwise(n){
+  if( n < 1 ) return false;
+
+  return ( n & (n-1) ) === 0;
+}
+
+/**
+ * Returns the value of the fibonacci sequence at the given index.
+ * @param {number} n - a non-negative integer representing
+ * the index of the element.
+ * @returns 
+ */
+function recursiveFibonacci( n ){
+  if( n == 0 ){
+    return 0;
+  }
+
+  if( n == 1 ){
+    return 1;
+  }
+
+  // can be simplified to
+  // if ( n < 2 ) return n
+  
+  return (recursiveFibonacci( n - 1 ) + recursiveFibonacci( n - 2 ));
+}
+
+/**
+ * Returns the factorial of the number given as input
+ * @param {number} n - a non-negative integer
+ * @returns 
+ */
+function recursiveFactorial( n ){
+  if( n == 0 ){
+    return 1;
+  }
+
+  return n * recursiveFactorial( n - 1 );
+}
+
+/**
+ * Returns the index of the target within the array given or
+ * -1 if it is not found
+ * @param {(number|Array)} array - an array of items
+ * @param {*} target - the value of the item to check for 
+ * @returns 
+ */
+function linearSearch( array, target ){
+  for( let i = 0; i < array.length; i++ ){
+    if( array[i] === target ) return i
+  }
+
+  return -1;
+}
+
+/** Returns the index of the target element within a
+ * sorted array, if not present returns -1.
+ * @param {Array.<number>} array 
+ * @param {number} target 
+ * @return {number}
+ */
+function binarySearch(array, target) {
+  
+  let start = 0;
+  let end = array.length - 1;
+
+  while( start <= end ){
+    // find middle element
+    let root = Math.floor( start + end ) / 2;
+
+    // check if root element is equal to target
+    if( target == array[root] ) return root;
+
+    // check if target is smaller or greater than root
+    if( target < array[root] ){
+      end = root - 1;
+    }
+    else{
+      start = root + 1;
+    }
+  }
+
+  return -1;
+}
+
 export {
   getFibonnaciSequence,
   getFactorial,
-  isPrimeNumber
+  isPrimeNumber,
+  isPowerOfTwo,
+  isPowerOfTwoBitwise,
+  recursiveFibonacci,
+  recursiveFactorial,
+  linearSearch,
+  binarySearch
 }
--- main.mjs
+++ main.mjs
@@ -1,10 +1,10 @@
 import * as DS from "./data_structures.mjs";
 import * as ALG from "./algorithms.mjs";
 
-let values = [ 23, -25, 0, 1, 2, 4, 5, NaN ];
+let values = [ 23, 0, 1, 2, 4, 5, 6, 64 ];
 values.forEach(
   ( value ) => {
-    console.log( `isPrime( ${value} ) => ${ ALG.isPrimeNumber( value ) }` );
+    console.log( `RecursiveFibonacci( ${value} ) => ${ ALG.recursiveFibonacci( value ) }` );
     return;
   }
 )
